f0fc343d61f97f6b0cd5b22cdd51001b88a84960
Nikita Kazantsev <65646389+NilTronn@users.noreply.github.com>
2020-07-22 19:23:29 +0300

    Add files via upload

---
+++ precache-manifest.cb3b7f37013fb3e05570cfdf951f1bea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97e5275a5a7261e79617f4fa16e80a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a7c14a68caeb2a03ed5",
+    "url": "/static/css/2.b9d9fd7e.chunk.css"
+  },
+  {
+    "revision": "e5ab602c9d9a082b15d4",
+    "url": "/static/css/main.c0f6f770.chunk.css"
+  },
+  {
+    "revision": "8a7c14a68caeb2a03ed5",
+    "url": "/static/js/2.1f19d2be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1f19d2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ab602c9d9a082b15d4",
+    "url": "/static/js/main.60563f96.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "5e0cfa56ca4b74b0ff3928431d79cb1c",
+    "url": "/static/media/bg.5e0cfa56.png"
+  },
+  {
+    "revision": "95b5f63a30d49ba1216d5f35e9346bdc",
+    "url": "/static/media/head_no_bg-min.95b5f63a.png"
+  }
+]);
